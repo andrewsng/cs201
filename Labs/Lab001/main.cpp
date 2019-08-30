3f56eeb24324f0252a183f357acc86209897bdd6
Andrew Ng <ngandrew1337@gmail.com>
2019-08-30 10:10:53 -0800

    Adding source code for Hello World program

---
+++ Labs/Lab001/main.cpp
@@ -0,0 +1,14 @@
+/*
+main.cpp
+Andrew Ng
+8 Aug 2019
+Hello World and arithmetic program for CS201 Lab 1
+*/
+
+#include<iostream>
+
+int main() {
+	std::cout << "Hello, world!\n";
+	std::cout << "This is on different line" << std::endl;
+	return 0;
+}

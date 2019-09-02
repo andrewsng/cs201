216eef1a8c506c2d6f1bbea2b28724113ec7dc9c
Andrew Ng <ngandrew1337@gmail.com>
2019-09-01 22:58:25 -0800

    Adding Hello World and Poem programs for hw0

---
+++ hw0/poem/poem.cpp
@@ -0,0 +1,16 @@
+/*
+	poem.cpp
+	Andrew Ng
+	1 Sep 2019
+	Poem excerpt program for hw0
+*/
+
+#include <iostream>
+
+int main()
+{
+	std::cout << "And on the pedestal these words appear: \n"
+				 "'My name is Ozymandias, king of kings: \n"
+				 "Look on my works, ye Mighty, and despair!'";
+	return 0;
+}

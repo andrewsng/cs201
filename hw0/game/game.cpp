a8a97ed5b0854ada22ef023ddf14425eca9e4852
Andrew Ng <ngandrew1337@gmail.com>
2019-09-01 23:29:35 -0800

    Adding extra info for poem excerpt and game description

--- hw0/game/game.cpp
+++ hw0/game/game.cpp
@@ -9,7 +9,8 @@
 
 int main()
 {
+	std::cout << "This is a description written by Grinding Gear Games for their video game, Path of Exile:" << std::endl;
 	std::cout << "Path of Exile is an online Action RPG set in the dark fantasy world of Wraeclast.\n"
 		"It is designed around a strong online item economy, deep character customisation, competitive PvP and ladder races.\n"
-		"The game is completely free and will never be 'pay to win.'";
+		"The game is completely free and will never be 'pay to win.'" << std::endl;
 }
--- hw0/poem/poem.cpp
+++ hw0/poem/poem.cpp
@@ -9,8 +9,9 @@
 
 int main()
 {
+	std::cout << "This is an excerpt from the poem Ozymandias by Percy Bysshe Shelley:" << std::endl;
 	std::cout << "And on the pedestal these words appear: \n"
-				 "'My name is Ozymandias, king of kings: \n"
-				 "Look on my works, ye Mighty, and despair!'";
+		"'My name is Ozymandias, king of kings: \n"
+		"Look on my works, ye Mighty, and despair!'" << std::endl;
 	return 0;
 }

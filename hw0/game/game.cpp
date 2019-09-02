0c40368299428318dbea2498ca90a158d1677e38
Andrew Ng <ngandrew1337@gmail.com>
2019-09-01 23:21:33 -0800

    Adding a game description program for hw0

---
+++ hw0/game/game.cpp
@@ -0,0 +1,15 @@
+/*
+	game.cpp
+	Andrew Ng
+	1 Sep 2019
+	Game description program for hw0
+*/
+
+#include <iostream>
+
+int main()
+{
+	std::cout << "Path of Exile is an online Action RPG set in the dark fantasy world of Wraeclast.\n"
+		"It is designed around a strong online item economy, deep character customisation, competitive PvP and ladder races.\n"
+		"The game is completely free and will never be 'pay to win.'";
+}

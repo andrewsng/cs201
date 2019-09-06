8a959b248358cb0f541eb6652ab6325a273f67ad
Andrew Ng <ngandrew1337@gmail.com>
2019-09-06 09:52:56 -0800

    Adding Lab 2 Part 3

--- Labs/Lab002/Birthday/birthday.cpp
+++ Labs/Lab002/Birthday/birthday.cpp
@@ -20,6 +20,35 @@ int main()
 	cin >> day;
 	cout << "Enter the year of your birthday as an integer." << endl;
 	cin >> year;
+	cout << "Your birthday is: " << year << "-";
+	if (month < 10) {
+		cout << "0" << month << "-";
+	}
+	else {
+		cout << month << "-";
+	}
+	if (day < 10) {
+		cout << "0" << day << endl;
+	}
+	else {
+		cout << day << endl;
+	}
+	return 0;
+}
+
+/*
+PART 1
+--------------------
+int main()
+{
+	int month, day, year;
+	cout << "Enter the month of your birthday as an integer." << endl;
+	cin >> month;
+	cout << "Enter the day of your birthday as an integer." << endl;
+	cin >> day;
+	cout << "Enter the year of your birthday as an integer." << endl;
+	cin >> year;
 	cout << "Your birthday is: " << month << "/" << day << "/" << year << endl;
 	return 0;
 }
+*/

3aa6c788b50fd76bdf2f8698f6f5a6061c0de48f
Andrew Ng <ngandrew1337@gmail.com>
2019-09-09 11:27:45 -0800

    Adding diamond printing program

--- hw01/diamond/diamond.cpp
+++ hw01/diamond/diamond.cpp
@@ -7,9 +7,19 @@ using std::endl;
 int main()
 {
 	int diamondSize;
-	int numLines;
 	int numColumns;
 	cout << "Please enter a positive integer!:" << endl;
 	cin >> diamondSize;
-	numLines = (diamondSize * 2) - 1;
+	if (diamondSize <= 0 || diamondSize)
+	numColumns = (diamondSize * 2) - 1;
+	for (int i = -(diamondSize - 1); i < diamondSize; i++) {
+		for (int j = 0; j < abs(i); j++) {
+			cout << " ";
+		}
+		for (int k = 0; k < numColumns - (2*abs(i)); k++) {
+			cout << "#";
+		}
+		cout << endl;
+	}
+	return 0;
 }

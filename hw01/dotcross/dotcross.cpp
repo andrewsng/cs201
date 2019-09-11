d7e22843e67f7e9eea750fe9e2bce999007b3c85
Andrew Ng <ngandrew1337@gmail.com>
2019-09-11 10:08:26 -0800

    Adding dotcross program for hw1

---
+++ hw01/dotcross/dotcross.cpp
@@ -0,0 +1,17 @@
+/*
+	dotcross.cpp
+	Andrew Ng
+	Sep 11 2019
+	Dot product and cross product program for hw1
+*/
+#include <iostream>
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+int main()
+{
+
+	return 0;
+}

607b151ad24dd5ff55e45552c27931177113047b
Andrew Ng <ngandrew1337@gmail.com>
2019-09-15 18:14:57 -0800

    Adding code for cross/dot product

--- hw01/dotcross/dotcross.cpp
+++ hw01/dotcross/dotcross.cpp
@@ -1,10 +1,11 @@
 /*
 	dotcross.cpp
 	Andrew Ng
-	Sep 11 2019
+	Sep 15 2019
 	Dot product and cross product program for hw1
 */
 #include <iostream>
+#include <iomanip>
 
 using std::cin;
 using std::cout;
@@ -12,6 +13,17 @@ using std::endl;
 
 int main()
 {
-
+	float Ax, Ay, Az;
+	float Bx, By, Bz;
+	float Cx, Cy, Cz;
+	cout << "Enter the three components of your first vector, A, separately." << endl;
+	cin >> Ax >> Ay >> Az;
+	cout << "Enter the three components of your second vector, B, separately." << endl;
+	cin >> Bx >> By >> Bz;
+	Cx = (Ay * Bz) - (Az * By);
+	Cy = (Az * Bx) - (Ax * Bz);
+	Cz = (Ax * By) - (Ay * Bx);
+	cout << std::setprecision(5) << std::fixed << "A dot B = " << (Ax * Bx) + (Ay * By) + (Az * Bz) << endl;
+	cout << std::setprecision(5) << std::fixed << "A cross B = <" << Cx << ", " << Cy << ", " << Cz << ">" << endl;
 	return 0;
 }

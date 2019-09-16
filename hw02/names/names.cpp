c04ef63609ee52040f440e1c57d19a3a65b5e5c0
Andrew Ng <ngandrew1337@gmail.com>
2019-09-16 11:19:09 -0800

    Adding 10 names source file

---
+++ hw02/names/names.cpp
@@ -0,0 +1,19 @@
+/*
+	names.cpp
+	Andrew Ng
+	Sep 16 2019
+	10 Names program for hw2
+*/
+
+#include <iostream>
+#include <vector>
+#include <string>
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+int main()
+{
+
+}

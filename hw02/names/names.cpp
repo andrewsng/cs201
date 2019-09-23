857c6b97f42f2d75ba500e4460218d6fea04cee0
Andrew Ng <ngandrew1337@gmail.com>
2019-09-23 09:11:19 -0800

    Revert "Adding code to InputNames function"
    This reverts commit 4937609f1afabe07d6f5cfa01adaca5ef8bce319.

--- hw02/names/names.cpp
+++ hw02/names/names.cpp
@@ -12,18 +12,10 @@
 using std::cin;
 using std::cout;
 using std::endl;
-using std::string;
-using std::vector;
 
 void InputNames()
 {
-	vector<string> names;
-	for (int i = 0; i < 10; i++) {
-		string name;
-		cout << "Please enter a name: " << endl;
-		std::getline(cin, name);
-		names.push_back(name);
-	}
+
 }
 
 int main()

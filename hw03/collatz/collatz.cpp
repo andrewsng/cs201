2fde13625312ffe774c525edd809b667f6e8c855
Andrew Ng <ngandrew1337@gmail.com>
2019-09-27 11:14:40 -0800

    Updating main and collatz. NEEDS FIXING

--- hw03/collatz/collatz.cpp
+++ hw03/collatz/collatz.cpp
@@ -11,25 +11,37 @@ using std::cin;
 using std::cout;
 using std::endl;
 
-int collatz(int& n)
+void collatz(int& n)
 {
-	if (n % 2 == 0) {
-		return (n / 2);
+	while (n != 1) {
+		if (n % 2 == 0) {
+			n = (n / 2);
+			cout << n << " ";
+		}
+		else {
+			n = (3 * n) + 1;
+			cout << n << " ";
+		}
 	}
-	else {
-		return (3 * n) + 1;
-	}
+	cout << n << endl;
 }
 
 int main()
 {
 	int n;
-	cin >> n;
 	while (true) {
-		cout << n << " ";
-		if (n == 1) {
+		cin >> n;
+		if (n == 0) {
 			break;
 		}
-		n = collatz(n);
+		else if (cin.fail() || n < 0) {
+			std::cerr << "You did not enter a positive integer. Try again." << endl;
+			cin.clear();
+			cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+			continue;
+		}
+		else {
+			collatz(n);
+		}
 	}
 }

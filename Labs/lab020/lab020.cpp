0c2c748f748b01067fb4a2fe92b4763c8c30e587
Andrew Ng <ngandrew1337@gmail.com>
2019-10-21 10:12:43 -0800

    adding lab 20

---
+++ Labs/lab020/lab020.cpp
@@ -0,0 +1,25 @@
+/*
+	lab020.cpp
+	Andrew Ng
+	Oct 21 2019
+	lab 20 for cs201
+*/
+
+#define _USE_MATH_DEFINES
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+
+using std::cout;
+using std::endl;
+using std::setw;
+using std::setprecision;
+
+int main()
+{
+	for (int i = 0; i <= 360; i += 5) {
+		cout << setw(3) << i << " "
+			<< setw(11) << setprecision(8) << std::fixed 
+			<< cos(i * (M_PI / 180.)) << endl;
+	}
+}

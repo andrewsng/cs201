fd9038d9194ef49f71b1baf9a346944a5698657c
Andrew Ng <ngandrew1337@gmail.com>
2019-11-06 10:16:15 -0900

    Adding lab 27

---
+++ Labs/Lab027/Lab027.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <random>
+
+using std::cout;
+using std::endl;
+using std::mt19937;
+using std::random_device;
+using std::uniform_int_distribution;
+
+int main()
+{
+	random_device rd;
+	mt19937 gen(rd());
+	uniform_int_distribution<int> distribCoin(0, 1);
+	int heads = 0;
+	int tails = 0;
+	for (int i = 0; i < 1000; ++i) {
+		if (distribCoin(gen) == 0) {
+			cout << "Heads" << endl;
+			heads++;
+		}
+		else {
+			cout << "Tails" << endl;
+			tails++;
+		}
+	}
+	cout << endl << heads << " Heads, and " << tails << " Tails." << endl;
+}

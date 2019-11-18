f1063cdd7d41bdb6c7d7cfec618aca424a1425cf
Andrew Ng <ngandrew1337@gmail.com>
2019-11-17 19:00:17 -0900

    Adding RandomBetween function and comments to random-map.cpp

--- hw06/random-map/random-map.cpp
+++ hw06/random-map/random-map.cpp
@@ -1,3 +1,11 @@
+/*
+random-map.cpp
+Andrew Ng
+Nov 17 2019
+random distribution tests for hw 6
+*/
+
+
 #include <iostream>
 #include <iomanip>
 #include <string>
@@ -11,11 +19,16 @@ using std::cout;
 using std::endl;
 
 
-int clamp(int x, int a, int b) {
+// Takes in an int and two bounds
+// Returns bounding value if outside of bounds
+int clamp(int x, int a, int b)
+{
 	return (x < a) ? (a) : (x > b) ? (b) : (x);
 }
 
 
+// Makes uniform distribution using given range
+// Returns one value generated with that distribution
 int RandomBetweenU(int first, int last)
 {
 	std::random_device rd;
@@ -25,6 +38,8 @@ int RandomBetweenU(int first, int last)
 }
 
 
+// Makes normal distribution using given range
+// Returns one value generated with that distribution
 int RandomBetweenN(int first, int last)
 {
 	std::random_device rd;
@@ -36,12 +51,17 @@ int RandomBetweenN(int first, int last)
 }
 
 
+// Returns value within a given range using rand() function
 int RandomBetween(int first, int last)
 {
-	return 0;
+	std::random_device rd;
+	std::srand(rd());
+	return rand() % (last - first + 1) + 1;
 }
 
 
+// Takes map storing how many times numbers were generated
+// Prints map out as a histogram to show distribution
 void PrintDistribution(const std::map<int, int>& numbers)
 {
 	for (auto p : numbers) {
@@ -52,40 +72,31 @@ void PrintDistribution(const std::map<int, int>& numbers)
 }
 
 
+// Generates maps to count generated numbers
+// Calls function to print the distributions
 int main()
 {
-	std::random_device rd;
-
-	std::default_random_engine e1(rd());
-	std::uniform_int_distribution<int> uniform_dist(1, 6);
-	int mean = uniform_dist(e1);
-	cout << "Randomly-chosen mean is " << mean << endl;
-
-	std::seed_seq seed2{ rd(), rd(), rd(), rd(), rd(), rd(), rd(), rd() };
-	std::mt19937 e2(seed2);
-	std::normal_distribution<> normal_dist(mean, 2);
-
-	std::map<int, int> hist;
-	for (int i = 0; i < 10000; ++i) {
-		++hist[std::round(normal_dist(e2))];
-	}
-	for (auto p : hist) {
-		std::cout << std::setw(2)
-			<< p.first << ' ' << std::string(p.second / 200, '*') << '\n';
-	}
 	std::map<int, int> histU;
-	for (int i = 0; i < 10000; ++i) {
-		++histU[std::round(RandomBetweenU(1, 6))];
+	for (int i = 0; i < 100000; ++i) {
+		++histU[std::round(RandomBetweenU(1, 20))];
 	}
+
 	std::map<int, int> histN;
-	for (int i = 0; i < 10000; ++i) {
-		++histN[std::round(RandomBetweenN(1, 6))];
+	for (int i = 0; i < 100000; ++i) {
+		++histN[std::round(RandomBetweenN(1, 20))];
 	}
+
 	std::map<int, int> histR;
-	for (int i = 0; i < 10000; ++i) {
-		++histR[std::round(RandomBetween(1, 6))];
+	for (int i = 0; i < 100000; ++i) {
+		++histR[std::round(RandomBetween(1, 20))];
 	}
+
+	cout << "Uniform distribution between 1 and 20: " << endl;
 	PrintDistribution(histU);
+
+	cout << "Normal distribution between 1 and 20: " << endl;
 	PrintDistribution(histN);
-	//PrintDistribution(histR);
+
+	cout << "rand() distribution between 1 and 20: " << endl;
+	PrintDistribution(histR);
 }

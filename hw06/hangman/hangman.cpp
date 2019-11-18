ab8989fcb8f5b25248a6b7bf5309404b1a9abc29
Andrew Ng <ngandrew1337@gmail.com>
2019-11-17 20:57:22 -0900

    Added win/lose conditions, upper/lowercase inputs, and guesses left to hangman.cpp

--- hw06/hangman/hangman.cpp
+++ hw06/hangman/hangman.cpp
@@ -2,6 +2,7 @@
 #include <map>
 #include <algorithm>
 #include <vector>
+#include <cctype>
 
 
 using std::cin;
@@ -14,25 +15,40 @@ using std::vector;
 int main()
 {
 	vector<char> answer{ 'h', 'o', 'w', 'd', 'y' };
+	std::unique(answer.begin(), answer.end());
+	std::for_each(answer.begin(), answer.end(),
+		[](char& c) {
+			c = std::toupper(c);
+		});
 	map<char, int> letters;
 	int wrong = 0;
-	while (wrong < 10) {
+	while (wrong <= 10) {
+		if (wrong == 10) {
+			cout << '\n' << "You lose!" << endl;
+			break;
+		}
 		char guess;
 		cin >> guess;
+		guess = std::toupper(guess);
 		if (letters.count(guess) != 0) {
 			cout << guess << " was already guessed." << endl;
+			cout << "You have " << 10 - wrong << " more guesses." << endl;
 			continue;
 		}
 		++letters[guess];
-		if (std::find(answer.begin(), answer.end(), guess) == answer.end()) {
-			cout << guess << " not found" << endl;
+		auto findIter = std::find(answer.begin(), answer.end(), guess);
+		if (findIter == answer.end()) {
+			cout << guess << " is not in the word." << endl;
 			wrong++;
+			cout << "You have " << 10 - wrong << " more guesses." << endl;
 		}
 		else {
 			cout << guess << " is correct." << endl;
+			answer.erase(findIter);
 		}
-	}
-	for (const auto m : letters) {
-		cout << m.first << ' ' << m.second << endl;
+		if (answer.empty() == 1) {
+			cout << '\n' << "You win!" << endl;
+			break;
+		}
 	}
 }

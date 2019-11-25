d66e038f3951117a7a845cd582fc36e3463f3def
Andrew Ng <ngandrew1337@gmail.com>
2019-11-24 21:41:02 -0900

    Adding spaces

--- hw07/asciiart/asciiart.cpp
+++ hw07/asciiart/asciiart.cpp
@@ -4,13 +4,13 @@
 #include "Color3.hpp"
 #include "Image3.hpp"
 
+
 using std::cout;
 using std::endl;
 using std::ifstream;
 using std::ofstream;
 using std::string;
 using std::vector;
-
 
 
 int main()
